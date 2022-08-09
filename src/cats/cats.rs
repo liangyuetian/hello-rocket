19a95ff6c042e14be61c00431550c7e29c8ec1cb
pt <pt_liangyue@outlook.com>
2022-08-10 00:52:41 +0800

    演示如何返回数据，没有记录返回 stream

--- src/cats/cats.rs
+++ src/cats/cats.rs
@@ -1,8 +1,81 @@
-pub fn new() {
+use std::path::{Path, PathBuf};
+use rocket::http::{ContentType, Header, Status};
+use rocket::response::{content, status};
+use rocket::serde::{Serialize, Deserialize};
 
+#[get("/cats")]
+pub fn cats1() -> status::Accepted<String> {
+    status::Accepted(Some(format!("hello cats")))
 }
 
-#[get("/")]
-pub fn index() -> &'static str {
-    "Hello, world!"
+#[get("/cats2")]
+pub fn cats2() -> status::Custom<content::RawJson<&'static str>> {
+    status::Custom(Status::Ok, content::RawJson("{ \"hi\": \"cats2\" }"))
+}
+
+#[derive(Responder)]
+#[response(status = 201, content_type = "json")]
+pub struct RawTeapotJson(&'static str);
+
+#[get("/cats3")]
+pub fn cats3() -> RawTeapotJson {
+    RawTeapotJson("{ \"hi\": \"cats3\" }")
+}
+
+#[get("/cats4")]
+pub fn cats4() -> Status {
+    Status::NotAcceptable
+}
+
+// #[derive(Responder)]
+// #[response(status = 500, content_type = "json")]
+// struct MyResponder {
+//     // inner: OtherResponder,
+//     header: ContentType,
+//     more: Header<'static>,
+//     // #[response(ignore)]
+//     // unrelated: MyType,
+// }
+
+#[get("/cats5")]
+pub fn cats5() -> Status {
+    Status::NotAcceptable
+}
+use rocket::fs::NamedFile;
+use rocket::response::status::NotFound;
+use rocket::serde::json::{Json, Value};
+use rocket::serde::json::serde_json::json;
+
+// 使用 Option，错误时返回错误信息
+#[get("/cats6/<file..>")]
+pub async fn cats6(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).await.ok()
+}
+
+// 使用 Result 返回错误信息
+#[get("/cats7/<file..>")]
+pub async fn cats7(file: PathBuf) -> Result<NamedFile, NotFound<String>> {
+    let path = Path::new("static/").join(file);
+    NamedFile::open(&path).await.map_err(|e| NotFound(e.to_string()))
+}
+
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct Task {
+    id: u16,
+    message: String
+}
+
+#[get("/cats8")]
+pub fn cats8() -> Json<Task> {
+    Json(Task {
+        id: 0,
+        message: "cats8 的 id 为 0".to_string()
+    })
+}
+
+#[get("/cats9")]
+pub fn cats9() -> Value {
+    json!({ "status": "ok", "id": "9", "name": "cats9" })
 }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@
 mod cats;
 mod dogs;
 
-use std::io;
-
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
 
@@ -17,9 +15,19 @@ async fn main() -> Result<(), rocket::Error> {
         .mount("/", routes![dogs::dogs::delay])
         .mount("/", routes![dogs::dogs::blocking_task])
         .mount("/", routes![dogs::dogs::admin_panel_redirect])
+        .mount("/", routes![dogs::dogs::user_id])
         .mount("/", routes![dogs::dogs::login])
         .mount("/", routes![dogs::dogs::logout])
         .mount("/", routes![dogs::dogs::user])
+        .mount("/", routes![cats::cats::cats1])
+        .mount("/", routes![cats::cats::cats2])
+        .mount("/", routes![cats::cats::cats3])
+        .mount("/", routes![cats::cats::cats4])
+        .mount("/", routes![cats::cats::cats5])
+        .mount("/", routes![cats::cats::cats6])
+        .mount("/", routes![cats::cats::cats7])
+        .mount("/", routes![cats::cats::cats8])
+        .mount("/", routes![cats::cats::cats9])
         .launch()
         .await?;
 
